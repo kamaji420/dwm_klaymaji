a0c6bc3d56a3c2850bdbb69304c19add8cdb76f5
kamaji420 <greenshark33@gmail.com>
2024-01-06 00:08:23 -0500

    changed more keybindings

--- config.h
+++ config.h
@@ -1,5 +1,8 @@
 /* See LICENSE file for copyright and license details. */
 
+/* constants */
+#define TERMINAL "kitty"
+
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const int startwithgaps	    = 1;	/* 1 means gaps are used by default */
@@ -9,7 +12,6 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char dmenuprompt[]     = "klaymaji<3";
 static const char col_gray1[]       = "#362c34";
 static const char col_gray2[]       = "#f3c6c6";
 static const char col_purple[]      = "#d9a2fb";
@@ -58,43 +60,57 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
+
+static const char dmenuprompt[] = "klaymaji<3";
+static const char n3sort[] = "v";
+static const char plugstart[] = "p";
+
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_pink, "-sb", col_purple, "-sf", col_gray1, "-i", "-p", dmenuprompt, NULL };
-static const char *termcmd[]  = { "kitty", NULL };
+static const char *pmixer[] = { TERMINAL, "pulsemixer", NULL };
+static const char *n3[] = { TERMINAL, "nnn", "-aCDEHU", "-T", n3sort, "-P", plugstart, NULL };
+static const char *up_vol[] = { "pactl", "set-sink-volume", "@DEFAULT_SINK@", "+1%",   NULL };
+static const char *down_vol[] = { "pactl", "set-sink-volume", "@DEFAULT_SINK@", "-1%",   NULL };
+static const char *mute_vol[] = { "pactl", "set-sink-mute",   "@DEFAULT_SINK@", "toggle", NULL };
 
 #include "movestack.c"
+#include <X11/XF86keysym.h>
 static const Key keys[] = {
-	/* modifier                     key             function        argument */
-	{ MODKEY,                       XK_r,           spawn,          {.v = dmenucmd } },
-	{ MODKEY,                       XK_t,           spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,           togglebar,      {0} },
-	{ MODKEY,                       XK_Down,        focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_Up,          focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_Left,        incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_Right,       incnmaster,     {.i = -1 } },
-	{ MODKEY|ControlMask,           XK_Left,        setmfact,       {.f = -0.05} },
-	{ MODKEY|ControlMask,           XK_Right,       setmfact,       {.f = +0.05} },
-	{ MODKEY|ControlMask,           XK_Down,        movestack,      {.i = +1 } },
-	{ MODKEY|ControlMask,           XK_Up,          movestack,      {.i = -1 } },
-	{ MODKEY,                       XK_Return,      zoom,           {0} },
-	{ MODKEY,                       XK_Tab,         view,           {0} },
-	{ MODKEY|ControlMask,           XK_d,           killclient,     {0} },
-	{ MODKEY|ControlMask,           XK_KP_Divide,   setlayout,      {.v = &layouts[0]} },
-	{ MODKEY|ControlMask,           XK_KP_Multiply, setlayout,      {.v = &layouts[1]} },
-	{ MODKEY|ControlMask,           XK_KP_Subtract, setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,       setlayout,      {0} },
-	{ MODKEY,                       XK_KP_Delete,   togglefloating, {0} },
-	{ MODKEY,                       XK_KP_Insert,   togglefullscr,  {0} },
-	{ MODKEY,                       XK_0,           view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,           tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,       focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period,      focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,       tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period,      tagmon,         {.i = +1 } },
-	{ MODKEY,                       XK_KP_Subtract, setgaps,        {.i = -5 } },
-	{ MODKEY,                       XK_KP_Add,      setgaps,        {.i = +5 } },
-	{ MODKEY,                       XK_KP_Divide,   setgaps,        {.i = GAP_RESET } },
-	{ MODKEY,                       XK_KP_Multiply, setgaps,        {.i = GAP_TOGGLE} },
+	/* modifier                     key                      function        argument */
+	{ MODKEY,                       XK_r,                    spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_t,                    spawn,          {.v = TERMINAL } },
+	{ MODKEY,                       XK_v,                    spawn,          {.v = pmixer } },
+	{ MODKEY,                       XK_e,                    spawn,          {.v = n3 } },
+	{ MODKEY,                       XK_b,                    togglebar,      {0} },
+	{ MODKEY,                       XK_Down,                 focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_Up,                   focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_Left,                 incnmaster,     {.i = +1 } },
+	{ MODKEY,                       XK_Right,                incnmaster,     {.i = -1 } },
+	{ MODKEY|ControlMask,           XK_Left,                 setmfact,       {.f = -0.05} },
+	{ MODKEY|ControlMask,           XK_Right,                setmfact,       {.f = +0.05} },
+	{ MODKEY|ControlMask,           XK_Down,                 movestack,      {.i = +1 } },
+	{ MODKEY|ControlMask,           XK_Up,                   movestack,      {.i = -1 } },
+	{ MODKEY,                       XK_space,                zoom,           {0} },
+	{ MODKEY,                       XK_Tab,                  view,           {0} },
+	{ MODKEY,                       XK_d,                    killclient,     {0} },
+	{ MODKEY|ControlMask,           XK_KP_Divide,            setlayout,      {.v = &layouts[0]} },
+	{ MODKEY|ControlMask,           XK_KP_Multiply,          setlayout,      {.v = &layouts[1]} },
+	{ MODKEY|ControlMask,           XK_KP_Subtract,          setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_KP_Delete,            togglefloating, {0} },
+	{ MODKEY,                       XK_KP_Insert,            togglefullscr,  {0} },
+	{ MODKEY,                       XK_0,                    view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,                    tag,            {.ui = ~0 } },
+	{ MODKEY,                       XK_comma,                focusmon,       {.i = -1 } },
+	{ MODKEY,                       XK_period,               focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_comma,                tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_period,               tagmon,         {.i = +1 } },
+	{ MODKEY,                       XK_KP_Subtract,          setgaps,        {.i = -5 } },
+	{ MODKEY,                       XK_KP_Add,               setgaps,        {.i = +5 } },
+	{ MODKEY,                       XK_KP_Divide,            setgaps,        {.i = GAP_RESET } },
+	{ MODKEY,                       XK_KP_Multiply,          setgaps,        {.i = GAP_TOGGLE} },
+	{ 0,                            XF86XK_AudioMute,        spawn,          {.v = mute_vol } },
+	{ 0,                            XF86XK_AudioLowerVolume, spawn,          {.v = down_vol } },
+	{ 0,                            XF86XK_AudioRaiseVolume, spawn,          {.v = up_vol } },
 	TAGKEYS(                        XK_KP_End,                 0)
 	TAGKEYS(                        XK_KP_Down,                1)
 	TAGKEYS(                        XK_KP_Next,                2)
@@ -111,7 +127,7 @@ static const Button buttons[] = {
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
-	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
+	{ ClkStatusText,        0,              Button2,        spawn,          {.v = TERMINAL } },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
